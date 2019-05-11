9e47b8cc96023a7dbf25de073bc158f440fc38ec
alkresin <alkresin@yahoo.com>
2019-05-11 14:30:52 +0300

    bld_plugins.sh added

---
+++ bld_plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export HB_INS="/opt/harbour"
+
+cd source/plugins
+$HB_INS/bin/linux/gcc/harbour plug_c_spis.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_go_spis.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_go_fmt.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_go_run.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_prg_compile.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_prg_run.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_prg_spis.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_hbp_init.prg -n -gh
+cd ../../
+
