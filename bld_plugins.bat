e3777c4f57d3214fb0638c3acec32b0f9b7b3209
Alexander S.Kresin <alkresin@yahoo.com>
2019-05-16 10:19:16 +0300

    plugins build scripts updated

--- bld_plugins.bat
+++ bld_plugins.bat
@@ -6,6 +6,7 @@ harbour plug_c_spis.prg -n -gh
 harbour plug_go_spis.prg -n -gh
 harbour plug_go_fmt.prg -n -gh
 harbour plug_go_run.prg -n -gh
+harbour plug_go_build.prg -n -gh
 harbour plug_prg_compile.prg -n -gh
 harbour plug_prg_run.prg -n -gh
 harbour plug_prg_spis.prg -n -gh
--- bld_plugins.sh
+++ bld_plugins.sh
@@ -6,6 +6,7 @@ $HB_INS/bin/linux/gcc/harbour plug_c_spis.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_go_spis.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_go_fmt.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_go_run.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_go_build.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_prg_compile.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_prg_run.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_prg_spis.prg -n -gh
