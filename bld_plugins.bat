1b8683ba09092bfdc3819f96dc3c7dcab65cb5bf
alkresin <alkresin@yahoo.com>
2019-05-30 21:40:49 +0300

    chartable plugin added to ini and build scripts

--- bld_plugins.bat
+++ bld_plugins.bat
@@ -11,5 +11,6 @@ harbour plug_prg_compile.prg -n -gh
 harbour plug_prg_run.prg -n -gh
 harbour plug_prg_spis.prg -n -gh
 harbour plug_hbp_init.prg -n -gh
+harbour plug_chartable.prg -n -gh
 cd ..\..\
 
--- bld_plugins.sh
+++ bld_plugins.sh
@@ -11,5 +11,6 @@ $HB_INS/bin/linux/gcc/harbour plug_prg_compile.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_prg_run.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_prg_spis.prg -n -gh
 $HB_INS/bin/linux/gcc/harbour plug_hbp_init.prg -n -gh
+$HB_INS/bin/linux/gcc/harbour plug_chartable.prg -n -gh
 cd ../../
 
