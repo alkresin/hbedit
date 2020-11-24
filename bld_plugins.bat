b5e86d7d558065d50b7db851b7961eded3a2604a
alkresin <alkresin@debi.alx>
2020-11-24 19:23:50 +0300

    Fixes.

--- bld_plugins.bat
+++ bld_plugins.bat
@@ -15,7 +15,7 @@ harbour plug_prg_init.prg -n -gh
 harbour plug_prg_run.prg -n -gh
 harbour plug_prg_spis.prg -n -gh
 harbour plug_hbp_init.prg -n -gh
-harbour plug_lisp_run.prg -n -gh
+harbour plug_lisp_init.prg -n -gh
 harbour plug_selection.prg -n -gh
 harbour plug_chartable.prg -n -gh
 harbour plug_calculator.prg -n -gh
